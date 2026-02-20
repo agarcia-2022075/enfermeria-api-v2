823a293a7d3db2fabf86fe235d82c89b4dbda355
agarcia-2022075 <agarcia-2022075@kinal.edu.gt>
2026-02-19 18:28:01 -0600

    feat: agregando esqueleto inicial de Spring Boot

---
+++ src/test/java/com/enfermeria/api/EnfermeriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enfermeria.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnfermeriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
