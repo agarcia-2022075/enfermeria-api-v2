ef87525b5eb117da91140a1c79f28c8a1ba34766
hgarcia-2023541 <hgarcia-2023541@kinal.edu.gt>
2026-02-19 19:28:36 -0600

    feat(repositorio): se agrega PacienteRepository extendiendo JpaRepository

--- src/main/java/com/enfermeria/api/repository/PacienteRepository.java
+++ src/main/java/com/enfermeria/api/repository/PacienteRepository.java
@@ -1,4 +1,10 @@
 package com.enfermeria.api.repository;
 
-public interface PacienteRepository {
+import com.enfermeria.api.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    List<Paciente> findByNombreContainingIgnoreCase(String nombre);
 }
