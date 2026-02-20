2c2e017cdcf855ff3a926a85eae1a3f5fe8889c3
jjax-2025012 <jjax-2025012@kinal.edu.gt>
2026-02-19 20:11:13 -0600

    feat: agregar entidad Insumo

---
+++ src/main/java/com/enfermeria/api/EnfermeriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.enfermeria.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnfermeriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnfermeriaApiApplication.class, args);
+	}
+
+}
